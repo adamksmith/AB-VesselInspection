94bbccbd4ae84f7cfd2e1545793cef5471754801
Adam <Adam.Smith@anheuser-busch.com>
2019-08-09 09:31:48 -0600

    Rev 1.1 - Added SQL Support

---
+++ src/SQLInterface.java
@@ -0,0 +1,51 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SQLInterface {
+    private Connection connection = null;
+    private ResultSet resultSet = null;
+    private Statement statement = null;
+
+    public SQLInterface(String file) {
+
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + file);
+            System.out.println("Connected");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeData(String state) {
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(state);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public ResultSet readData(String state) {
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(state);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+}
+
--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,19 @@
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class Tester {
     public static void main(String args[]) {
-        ConnectSQLite sql = new ConnectSQLite("INSERT INTO vesselInspection VALUES ('Chips2005', 'InspectionThing', 'PASS', 'Philip', CURRENT_DATE, 6766)");
+        UUID uuid = UUID.randomUUID();
+        String randomUUIDString = uuid.toString();
+        String str = "INSERT INTO vesselInspection VALUES ('BK','2009', 'ColdSide', 'Is it running'" +
+                ", 'NO', 'Philip', CURRENT_DATE, '" + randomUUIDString + "')";
+        String str1 = "SELECT * FROM vesselInspection WHERE VesselType LIKE 'BK'";
+        SQLInterface sql = new SQLInterface("H:\\Code\\DB\\vesselInspection.db");
+        sql.writeData(str);
+        try {
+            System.out.println(sql.readData(str1).getString("VesselID"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
